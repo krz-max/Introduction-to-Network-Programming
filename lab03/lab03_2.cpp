9d9d2808e36e841ca3ddbfee36524c3f2df8aaf5
krz-max <a1314kevin@gmail.com>
2022-10-09 21:41:49 +0800

    w

--- lab03/lab03_2.cpp
+++ lab03/lab03_2.cpp
@@ -35,29 +35,46 @@ int main(int argc, char *argv[]) {
     char garbage[1340];
     memset(garbage, 0, sizeof(garbage));
     int flag = 0;
-    struct timespec t = {0, 1};
+	int second = 0;
+	int bytepercycle = 0;
+	int BytesPerSecond = 0;
+    struct timespec t = {0, 1250000};
     if(strcmp(argv[1], "1") == 0){
-	flag = 0;
-        t.tv_nsec = 590000;
+		BytesPerSecond = 800000;
+		flag = 0;
     }
     else if(strcmp(argv[1], "1.5") == 0){
-	flag = 1;
-        t.tv_nsec =  370000;
+		BytesPerSecond = 1300000;
+		flag = 1;
     }
     else if(strcmp(argv[1], "2") == 0){
-	flag = 2;
-        t.tv_nsec = 508000;
+		BytesPerSecond = 1800000;
+		flag = 2;
     }
     else{
-	flag = 3;
-        t.tv_nsec = 500000;
+		BytesPerSecond = 2800000;
+		flag = 3;
     }
+	cout << flag;
 	gettimeofday(&_t0, NULL);
 	while(1) {
-		nanosleep(&t, NULL);
-		bytesent += send(sockfd, garbage, 1340, 0);
-		if(flag>=2) bytesent += send(sockfd, garbage, 1340, 0);
-		if(flag>=3) bytesent += send(sockfd, garbage, 1340, 0);
+		bytepercycle += send(sockfd, garbage, 1340, 0);
+		if(flag==1) bytepercycle += send(sockfd, garbage, 670, 0);
+		else if(flag==2) bytepercycle += send(sockfd, garbage, 1340, 0);
+		else if(flag==3){
+			bytepercycle += send(sockfd, garbage, 1340, 0);
+			bytepercycle += send(sockfd, garbage, 1340, 0);
+		}
+		second += t.tv_nsec;
+		if(second == 1000000000){
+			second = 0;
+			while(bytepercycle < BytesPerSecond-1340) {
+				bytepercycle += send(sockfd, garbage, 1340, 0);
+			}
+			bytepercycle += send(sockfd, garbage, BytesPerSecond-bytepercycle, 0);
+			bytepercycle = 0;
+		}
+		bytesent += bytepercycle;
 		nanosleep(&t, NULL);
 	}
 
